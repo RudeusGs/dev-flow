25d7fed40fcacf74d396d17deeba56ade99449f5
Ngô Trần Nguyên Quân <ngotrannguyenquan1908@gmail.com>
2026-06-02 10:54:29 +0700

    Init: backend project

---
+++ server/devflow-api/src/main/java/io/devflow/DevFlowApiApplication.java
@@ -0,0 +1,13 @@
+package io.devflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFlowApiApplication.class, args);
+	}
+
+}
